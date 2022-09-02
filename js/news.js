9a123e1b906fab829a3fcd3b0fb7f56e265e61ff
souvikhalder92 <107093196+souvikhalder92@users.noreply.github.com>
2022-09-03 02:20:16 +0600

    3rd

--- js/news.js
+++ js/news.js
@@ -1,1 +1,29 @@
-console.log('sd');
+const loadAllCategories = async() =>{
+    const url = `https://openapi.programming-hero.com/api/news/categories`;
+   const res =await fetch(url);
+   const data = await res.json();
+   setAllCategory(data.data.news_category);
+    
+}
+const setAllCategory = categorys =>{
+    //console.log(data);
+  
+    const allCategory = document.getElementById('all-category');
+     
+    
+      categorys.forEach(category =>{
+        console.log(category);
+        const div = document.createElement('div');
+        div.classList.add('a');
+        div.innerHTML = `
+        <a style="text-decoration: none" class="p-4 text-secondary">${category.category_name}</a>`;
+       allCategory.appendChild(div);
+        
+      });
+     
+      }
+
+
+
+//setAllMenu();
+loadAllCategories();
